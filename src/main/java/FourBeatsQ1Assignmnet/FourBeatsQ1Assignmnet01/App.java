1a44b95f359b2ae772a1161811bb8aedb2c9e820
IA-Istiak <istiakahamed616@gmail.com>
2024-09-04 22:11:29 +0600

    Commit one

---
+++ src/main/java/FourBeatsQ1Assignmnet/FourBeatsQ1Assignmnet01/App.java
@@ -0,0 +1,13 @@
+package FourBeatsQ1Assignmnet.FourBeatsQ1Assignmnet01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
